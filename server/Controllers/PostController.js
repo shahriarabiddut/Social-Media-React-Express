03a5a87e1ea887a2934537603a5a7ca635fc8c8c
shahriarabiddut <shahriarhmed@gmail.com>
2023-08-04 18:55:09 +0600

    REST API Complete

--- server/Controllers/PostController.js
+++ server/Controllers/PostController.js
@@ -1,4 +1,6 @@
+import mongoose from 'mongoose';
 import PostModel from '../Models/postModel.js';
+import UserModel from '../Models/userModel.js';
 
 //Create New Post
 export const createPost = async(req,res)=>{
@@ -65,3 +67,52 @@ export const deletePost = async (req,res) => {
 }
 
 //Like and Dislike a Post
+export const likePost = async (req,res)=>{
+    const id = req.params.id;
+    const {userId} = req.body;
+    try {
+        const post = await PostModel.findById(id);
+        if(!post.likes.includes(userId)){
+            await post.updateOne({$push : {likes : userId}});
+            res.status(200).json({message :"Post Liked!"});
+        }else{
+            await post.updateOne({$pull : {likes : userId}});
+            res.status(200).json({message :"Post Disliked!"});
+        }
+    } catch (error) {
+        res.status(500).json({message :error.message})
+    }
+}
+
+//Get Timeline Posts
+export const getTimelinePosts = async (req,res)=>{
+    const userId = req.params.id;
+    try {
+        const currentUserPost = await PostModel.find({userId : userId});
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match:{
+                    _id : new mongoose.Types.ObjectId(userId)
+                }
+            },{
+                $lookup:{
+                    from : "posts",
+                    localField : "following",
+                    foreignField : "userId",
+                    as : "followingPosts"
+                }
+            },{
+                $project :{
+                    followingPosts : 1,
+                    _id:0
+                }
+            }
+        ]);
+        res.status(200).json(currentUserPost.concat(...followingPosts[0].followingPosts).sort((a,b)=>{
+            return b.createdAt - a.createdAt;
+        }));
+        
+    } catch (error) {
+        res.status(500).json({message :error.message})
+    }
+}
